ed4f473c19d869525284ce3f439ede01ce157f1e
ant1 <l1htar@bk.ru>
2025-07-25 19:01:11 +0300

    init

---
+++ main.py
@@ -0,0 +1,11 @@
+from tasks_1.task1 import print_task1_start
+
+def print_start():
+    print('--> start...')
+
+
+if __name__ == "__main__":
+    print_start()
+
+    print_task1_start()
+        
---
+++ tasks_1/task1.py
@@ -0,0 +1,6 @@
+def print_task1_start():
+    print('--> task1 start...')
+
+
+if __name__ == "__main__":
+    print_task1_start()
