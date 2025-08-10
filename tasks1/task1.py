3adcd640d99f8a894edf1e4ba25f2774f13ada78
ant1 <l1htar@bk.ru>
2025-08-10 21:35:51 +0300

    переделал задачи

--- tasks1/task1.py
+++ tasks1/task1.py
@@ -6,21 +6,6 @@
 def is_odd(n: int) -> bool:
     return n % 2 == 0
 
-def is_odd2(n: int) -> bool:
-    return True if n % 2 == 0 else False
-
-def is_odd3(n: int) -> bool:
-    if n % 2 == 0:
-        return True
-    else:
-        return False
-
-def is_odd4(n: int) -> bool:
-    x = False
-    if n % 2 == 0:
-        x = True
-    return x
-
 if __name__ == "__main__":
 
     test_numbers = [10, 0, 7, 6, 14, -57]
